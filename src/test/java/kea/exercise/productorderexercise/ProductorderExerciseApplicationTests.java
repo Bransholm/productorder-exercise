b78a374239688b40bb6bbe9bba79585c08d9bff0
Bransholm <kenneth@bransholm.dk>
2024-04-11 17:21:14 +0200

    Project created

---
+++ src/test/java/kea/exercise/productorderexercise/ProductorderExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exercise.productorderexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductorderExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
